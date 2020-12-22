8fdfef2dbc37f498f45d3456453b99d0beb2b5d2
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2020-12-22 14:34:21 -0600

    starting enemy for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,17 @@ var playerMoney = 10;
 
 console.log(playerName);
 console.log(playerHealth, playerAttack, playerMoney);
-console.log("You can put regular sentences in these.");
 
-var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 
 // fight function
 var fight = function() {
@@ -24,20 +30,20 @@ var fight = function() {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -68,4 +74,4 @@ var fight = function() {
 };
  
 // run fight function to start game
-fight();
+// fight();
